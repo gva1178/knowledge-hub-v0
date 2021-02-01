51de5aae98126fce8f540028b17a6901f5819fcb
gva1178 <gva1178@gmail.com>
2021-01-31 20:43:48 -0500

    mongoDB integration not working right now -- saving before pivoting to using mongoDB straight up instead of with Monk

--- kHub_vMongo/routes/index.js
+++ kHub_vMongo/routes/index.js
@@ -8,4 +8,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: titleString });
 });
 
+/*GET test helloWorld page */
+router.get('/helloworld', function(req, res, next) {
+  res.render('helloworld', { title : "Hello world"});
+})
+
+/*GET test data entry page */
+router.get('/dbtestoutput', function(req, res, next) {
+  var db = req.db;
+  var collection = db.get('peoplecollection');
+  console.log("here");
+  //console.log(collection.find().then);
+  collection.find().toArray(function(e, items) {
+    if (err) throw err;
+    console.log(items);
+  });
+  collection.find({}, {}, function(e,docs) {
+    res.render('dbtestoutput', {
+      title : "Test output",
+      "peopleDatabase" : docs
+    });
+  });
+});
+
 module.exports = router;
