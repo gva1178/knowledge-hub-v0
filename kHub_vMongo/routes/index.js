546487a4e7b7bb52d76cbc4b9747554eeb7df6eb
gva1178 <gva1178@gmail.com>
2021-01-31 22:11:04 -0500

    Checkpoint: now able to add items to the people database through dbtestinput page, which redirects upon submission to dbtestoutput page, which in turn displays the contents of the database.

--- kHub_vMongo/routes/index.js
+++ kHub_vMongo/routes/index.js
@@ -19,7 +19,7 @@ router.get('/dbtestoutput', function(req, res, next) {
   var collection = db.get('peoplecollection');
   collection.find({}, {}, function(e,docs) {
     res.render('dbtestoutput', {
-      title : "Test output",
+      title : "Database output",
       "peopleDatabase" : docs
     });
   });
@@ -27,8 +27,38 @@ router.get('/dbtestoutput', function(req, res, next) {
 
 /*GET test data entry page */
 router.get('/dbtestinput', function(req, res, next) {
-  
+  res.render('dbtestinput', {
+    title : "Data input"
+  })
 });
 
+router.post('/addToDB', function(req, res) {
+
+  var db = req.db;
+
+  var userName = req.body.username;
+  var userRole = req.body.userrole;
+  var userImageURL = req.body.imageURL;
+  var userSkill1 = req.body.userSkill1;
+  var userSkill2 = req.body.userSkill2;
+  var userCampaign = req.body.usercampaign;
+
+  var collection = db.get('peoplecollection');
+
+  collection.insert({
+    name : userName,
+    role : userRole,
+    imageURL : userImageURL,
+    skill1 : userSkill1,
+    skill2 : userSkill2,
+    campaign : userCampaign
+  }, function (err, doc) {
+    if (err) {
+      res.send("Failed insertion into database")
+    } else {
+      res.redirect("dbtestoutput");
+    }
+  });
+});
 
 module.exports = router;
